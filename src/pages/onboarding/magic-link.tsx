7b07fac9d914709b69b1e8f2f0fe0d13e6686681
Mohit Mummon <mxkumar@protonmail.com>
2023-11-09 14:34:48 -0800

    update action code settings for email link

--- src/pages/onboarding/magic-link.tsx
+++ src/pages/onboarding/magic-link.tsx
@@ -38,7 +38,7 @@ const ContinueWithMagicLink = () => {
     const actionCodeSettings = {
         // URL you want to redirect back to. The domain (www.example.com) for this
         // URL must be in the authorized domains list in the Firebase Console.
-        url: 'http://reeltalk-web.vercel.app/onboarding/birthday',
+        url: 'https://reeltalk-web.vercel.app/onboarding/birthday',
         // This must be true.
         handleCodeInApp: true,
     };
