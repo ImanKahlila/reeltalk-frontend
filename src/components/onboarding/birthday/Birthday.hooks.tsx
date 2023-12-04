703ded3bab5609ea49f73384c2b9b5e212176507
Mohit Mummon <mxkumar@protonmail.com>
2023-12-04 12:53:28 -0800

    Validate birthdate so that age is between 13 and 100 years
    Extract birthday validation into custom hook

--- src/components/onboarding/birthday/Birthday.hooks.tsx
+++ src/components/onboarding/birthday/Birthday.hooks.tsx
@@ -1,6 +1,9 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 
+// Util
+import { extractDateValues } from './Birthday';
+import { isExists, subYears, isAfter, isBefore, isEqual } from 'date-fns';
 import toast from 'react-hot-toast';
 
 // Firebase
@@ -68,4 +71,55 @@ export const usePasswordlessSignin = () => {
     passwordlessSignIn();
     //eslint-disable-next-line
   }, []);
+};
+
+export const useValidateBirthday = () => {
+  const [dayValue, setDayValue] = useState('');
+  const [monthValue, setMonthValue] = useState('');
+  const [yearValue, setYearValue] = useState('');
+
+  const [birthdayValid, setBirthdayValid] = useState<boolean>(false);
+
+  function validateBirthday() {
+    const { year, month, day } = extractDateValues(yearValue, monthValue, dayValue);
+
+    // Check if the date exists
+    const isValidDate = isExists(year, month, day);
+
+    // Check if the date is within the specified range
+    const currentDate = new Date();
+    const minDate = subYears(currentDate, 101); // 101 years ago from now
+    const maxDate = subYears(currentDate, 13); // 13 years ago from now
+
+    const isAfterMinDate = isAfter(new Date(year, month, day), minDate);
+    const isBeforeMaxDate =
+      isBefore(new Date(year, month, day), maxDate) || isEqual(new Date(year, month, day), maxDate);
+
+    const isWithinRange = isValidDate && isAfterMinDate && isBeforeMaxDate;
+
+    setBirthdayValid(isWithinRange);
+  }
+
+  useEffect(() => {
+    validateBirthday();
+    //eslint-disable-next-line
+  }, [dayValue, monthValue, yearValue]);
+
+  function inputChangeHandler(inputType: string, value: string) {
+    switch (inputType) {
+      case 'day':
+        setDayValue(value);
+        break;
+      case 'month':
+        setMonthValue(value);
+        break;
+      case 'year':
+        setYearValue(value);
+        break;
+      default:
+        break;
+    }
+  }
+
+  return { birthdayValid, inputChangeHandler, yearValue, monthValue, dayValue };
 };
