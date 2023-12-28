a1e3db0c02a628d8b4d4d981a62fc7cad95cf6b0
Mohit Mummon <mxkumar@protonmail.com>
2023-12-28 11:38:41 -0800

    Update code to make request to backend for join requests for a community

--- src/pages/community/[communityId]/admin.tsx
+++ src/pages/community/[communityId]/admin.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
+import { useRouter } from 'next/router';
 import { NextPageContext } from 'next';
-import { doc, getDoc, getFirestore } from 'firebase/firestore';
-import app from '@/firebase/firebase-config';
-const db = getFirestore(app);
-import { ICommunityObject } from '.';
 import axios from 'axios';
 import nookies from 'nookies';
 import { useUserContext } from '@/lib/context';
 
+type JoinRequestData = {
+  displayName: string;
+  userId: string;
+}[];
+
 const CommunityAdminPage: React.FC<{ data?: string }> = ({ data }) => {
-  const pageData: ICommunityObject = data ? JSON.parse(data) : null;
+  const { query } = useRouter();
+  const { communityId } = query;
+  const joinRequestsData: JoinRequestData | null = data ? JSON.parse(data).validUserData : null;
   const { idToken } = useUserContext();
 
   async function approveRequest(request: string, action: boolean) {
     let response;
     try {
       response = await axios.post(
-        `http://localhost:8080/communities/join-community/${pageData.communityId}/pendingRequest`,
+        `http://localhost:8080/communities/join-community/${communityId}/pendingRequest`,
         { action: action, requestedUserId: request },
         {
           headers: { Authorization: `Bearer ${idToken}` },
@@ -31,24 +35,25 @@ const CommunityAdminPage: React.FC<{ data?: string }> = ({ data }) => {
   return (
     <div className='flex flex-col gap-4'>
       <h1 className='text-primary'>Requests to Join</h1>
-      {pageData.joinRequests?.map(request => (
-        <div key={request} className='flex items-center gap-4'>
-          <div className='text-medium-emphasis'>{request}</div>
+      {joinRequestsData?.map(request => (
+        <div key={request.userId} className='flex items-center gap-4'>
+          <div className='text-high-emphasis'>{request.displayName}</div>
+          <div className='text-medium-emphasis'>{request.userId}</div>
           <button
-            onClick={() => approveRequest(request, true)}
+            onClick={() => approveRequest(request.userId, true)}
             className='rounded-sm bg-primary px-3 py-1 font-medium transition-colors duration-300 hover:bg-opacity-80'
           >
             Approve
           </button>
           <button
-            onClick={() => approveRequest(request, false)}
+            onClick={() => approveRequest(request.userId, false)}
             className='rounded-sm bg-red-700 px-3 py-1 font-medium text-pure-white transition-colors duration-300 hover:bg-opacity-80'
           >
             Deny
           </button>
         </div>
       ))}
-      {!pageData?.joinRequests?.length && (
+      {!joinRequestsData?.length && (
         <p className='text-medium-emphasis'>No join requests at the moment :(</p>
       )}
     </div>
@@ -61,33 +66,15 @@ export async function getServerSideProps(context: NextPageContext) {
   const communityId = context.query.communityId;
   const idToken = nookies.get(context).idToken;
 
-  // TODO: Implement fetching from the backend
   let response;
   try {
     response = await axios.get(`http://localhost:8080/communities/join-community/${communityId}`, {
       headers: { Authorization: `Bearer ${idToken}` },
     });
 
-    console.log(response.data);
-  } catch (error: any) {
-    console.log(error.message);
-  }
-
-  // TODO: Remove code fetching from firestore directly
-  try {
-    const docRef = doc(db, `/communities/${communityId}`);
-    const docSnapShot = await getDoc(docRef);
-
-    if (!docSnapShot.exists()) {
-      return {
-        notFound: true,
-      };
-    }
-
     return {
       props: {
-        data: JSON.stringify(docSnapShot.data()),
-        notFound: false,
+        data: JSON.stringify(response.data),
       },
     };
   } catch (error: any) {
