b16de4ca9d52918adb5449a4235376517414af8c
Pisgah <pisgah2002@gmail.com>
2024-03-10 10:27:37 -0400

    created waitlist page

---
+++ src/pages/waitlist/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import WaitListSection from '@/components/waitlist/WaitListSection';
+
+const WaitListPage = () => {
+  return (
+    <>
+      <WaitListSection />
+    </>
+  );
+};
+
+export default WaitListPage;
